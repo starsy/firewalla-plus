2b51f0764318c38703ed975a946ae7a1bfcf2647
Alex Song <starsy@gmail.com>
2017-12-30 17:16:24 +0000

    add dnsmasq local cache

--- extension/dnsmasq/dnsmasq.sh
+++ extension/dnsmasq/dnsmasq.sh
@@ -1,1 +1,3 @@
 sudo /home/pi/firewalla/extension/dnsmasq/dnsmasq.armv7l -k -x /home/pi/.firewalla/run/dnsmasq.pid -u pi -C /home/pi/firewalla/extension/dnsmasq/dnsmasq.conf -r /home/pi/.firewalla/run/dnsmasq.resolv.conf --local-service 
+sudo /home/pi/firewalla/extension/dnsmasq/dnsmasq.armv7l -k -x /home/pi/.firewalla/run/dnsmasq-local.pid -u pi -C /home/pi/firewalla/extension/dnsmasq/dnsmasq-local.conf -r /home/pi/.firewalla/run/dnsmasq.resolv.conf --local-service 
+
