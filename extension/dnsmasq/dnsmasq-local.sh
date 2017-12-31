ee3fb866da637d8c05de69244773e49569aca61c
Alex Song <starsy@gmail.com>
2017-12-31 16:36:32 +0000

    remove meaningless prefix

--- extension/dnsmasq/dnsmasq-local.sh
+++ extension/dnsmasq/dnsmasq-local.sh
@@ -1,3 +1,3 @@
 sudo /home/pi/firewalla/extension/dnsmasq/dnsmasq.armv7l -k -x /home/pi/.firewalla/run/dnsmasq-local.pid -u pi -C /home/pi/firewalla/extension/dnsmasq/dnsmasq-local.conf -r /home/pi/.firewalla/run/dnsmasq.resolv.conf --local-service 
 
-sudo \cp -f /home/pi/firewalla/extension/dnsmasq/no_truncate  /etc/dhcp/dhclient-enter-hooks.d/aa_no_truncate
+sudo \cp -f /home/pi/firewalla/extension/dnsmasq/no_truncate  /etc/dhcp/dhclient-enter-hooks.d/no_truncate
