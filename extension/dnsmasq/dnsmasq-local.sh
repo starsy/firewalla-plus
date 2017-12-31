665bdb7bfece63cf546c26032c6652f4e1d112ed
Alex Song <starsy@gmail.com>
2017-12-31 16:52:49 +0000

    pre-set truncate env

--- extension/dnsmasq/dnsmasq-local.sh
+++ extension/dnsmasq/dnsmasq-local.sh
@@ -1,3 +1,2 @@
 sudo /home/pi/firewalla/extension/dnsmasq/dnsmasq.armv7l -k -x /home/pi/.firewalla/run/dnsmasq-local.pid -u pi -C /home/pi/firewalla/extension/dnsmasq/dnsmasq-local.conf -r /home/pi/.firewalla/run/dnsmasq.resolv.conf --local-service 
 
-sudo \cp -f /home/pi/firewalla/extension/dnsmasq/no_truncate  /etc/dhcp/dhclient-enter-hooks.d/no_truncate
