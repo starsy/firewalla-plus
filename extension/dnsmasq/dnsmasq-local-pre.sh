4e8eaf97a85df98cf5101bc00da63fcecfb8e2f3
Alex Song <starsy@gmail.com>
2017-12-31 18:07:37 +0000

    better scripting

--- extension/dnsmasq/dnsmasq-local-pre.sh
+++ extension/dnsmasq/dnsmasq-local-pre.sh
@@ -1,1 +1,15 @@
-sudo cp /home/pi/firewalla/extension/dnsmasq/no_truncate /etc/dhcp/dhclient-enter-hooks.d/
+#!/bin/bash
+
+NO_TRUNCATE='export TRUNCATE_NAMESERVER_LIST_AFTER_LOOPBACK_ADDRESS=0'
+
+grep "$NO_TRUNCATE" /etc/dhcp/debug
+
+if [ $? -ne 0 ]; then
+  sudo sh -c "echo $NO_TRUNCATE >> /etc/dhcp/debug"
+  if [ $? -eq 0 ]; then
+    echo "NO_TRUNCATE IS SET" 
+    exit 0
+  fi
+fi
+
+echo "NO_TRUNCATE IS NOT SET"
