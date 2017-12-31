588668123d0ff99392ba937a5522516f43a633bb
Alex Song <starsy@gmail.com>
2017-12-31 10:23:39 +0000

    add local dns cache to resolve the UI timeout issue

---
+++ extension/dnsmasq/dnsmasq-local.sh
@@ -0,0 +1,1 @@
+sudo /home/pi/firewalla/extension/dnsmasq/dnsmasq.armv7l -k -x /home/pi/.firewalla/run/dnsmasq-local.pid -u pi -C /home/pi/firewalla/extension/dnsmasq/dnsmasq-local.conf -r /home/pi/.firewalla/run/dnsmasq.resolv.conf --local-service 
