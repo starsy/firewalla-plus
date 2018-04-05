83a041e7da2924d720638df3800146069d79f066
Alex Song <starsy@gmail.com>
2018-04-05 14:22:22 +0000

    no sudo in dnsmasq.sh

--- extension/dnsmasq/dnsmasq.sh
+++ extension/dnsmasq/dnsmasq.sh
@@ -1,1 +1,5 @@
-sudo /home/pi/firewalla/extension/dnsmasq/dnsmasq.armv7l -k -x /home/pi/.firewalla/run/dnsmasq.pid -u pi -C /home/pi/firewalla/extension/dnsmasq/dnsmasq.conf -r /home/pi/.firewalla/run/dnsmasq.resolv.conf --local-service 
+#!/bin/bash
+/home/pi/firewalla/extension/dnsmasq/dnsmasq.armv7l -k --clear-on-reload -u pi -C /home/pi/firewalla/extension/dnsmasq/dnsmasq.conf -r /home/pi/.firewalla/run/dnsmasq.resolv.conf --local-service &
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+for job in `jobs -p`; do wait $job; echo "$job exited"; done
